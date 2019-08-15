877c45f48c664d1d0e0919f1a78c2a00a81a9c3a
ad6395 <33325928+ad6395@users.noreply.github.com>
2019-08-14 20:20:25 -0400

    Form Validation

--- src/graph/AlgosProject.java
+++ src/graph/AlgosProject.java
@@ -8,58 +8,86 @@ package graph;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
 
 /**
  *
- * @author vighnesh
+ * @author Abhishek Dongare
  */
 public class AlgosProject {
 
-    	private static int Target=2400;
-	private static int noOfInputs=3;
-	private static int noOfParticles=10;
-	private static int maxVelocity = 10; // Maximum velocity change allowed.
-	private static int upperLimit=2000;
-	private static int lowerLimit=1000;
-	private static int noOfIterations=20;
-	private static int goldP=10;
-	private static int diamondP=10;
-	private static int mutualfundsP=10;
-	private static int gPeriod=5;
-	private static int dPeriod=5;
-	private static int mPeriod=5;
-	private static int bPeriod=5;
+    	private static int Target;
+	private static int noOfInputs;
+	private static int noOfParticles;
+	private static int maxVelocity =10; // Maximum velocity change allowed.
+	private static int upperLimit;
+	private static int lowerLimit;
+	private static int noOfIterations;
+	private static int goldP;
+	private static int diamondP;
+	private static int mutualfundsP;
+	private static int gPeriod;
+	private static int dPeriod;
+	private static int mPeriod;
+	private static int bPeriod;
     public static  int epoch = 0;
     public static int gBest = 0;
     
+	public AlgosProject(int target, int noOfInputs, int noOfParticles, int upperLimit, int lowerLimit, int noOfIterations,
+			int goldP, int diamondP, int mutualfundsP, int gPeriod, int dPeriod, int mPeriod, int bPeriod) {
+		this.Target = target;
+		this.noOfInputs = noOfInputs;
+		this.noOfParticles = noOfParticles;
+		this.upperLimit = upperLimit;
+		this.lowerLimit = lowerLimit;
+		this.noOfIterations = noOfIterations;
+		this.goldP = goldP;
+		this.diamondP = diamondP;
+		this.mutualfundsP = mutualfundsP;
+		this.gPeriod = gPeriod;
+		this.dPeriod = dPeriod;
+		this.mPeriod = mPeriod;
+		this.bPeriod = bPeriod;
+	}
+    
 
     public static ArrayList<Particle> particles = new ArrayList<Particle>();
+        private static  ArrayList<Integer> particles2 = new ArrayList<>();
+    private static ArrayList<Integer> particles3 = new ArrayList<>();
     static MainJFrame mj = new MainJFrame();
-
+    public static TreeMap<Integer, Integer> particleTreeMap = new TreeMap<>();
+    
     private static void initialize() {
-        for (int i = 0; i < noOfParticles; i++) {
-            Particle newParticle = new Particle();
-            int total = 0;
-            
-                newParticle.data(0, getRandomNumber(lowerLimit, upperLimit));
-                newParticle.data(2, getRandomNumber(newParticle.data(0), upperLimit));
-                newParticle.data(1, getRandomNumber(newParticle.data(0), newParticle.data(2)));
-                total = newParticle.data(0) + newParticle.data(1) + newParticle.data(2);
-             // j
-            newParticle.pBest(total);
-            particles.add(newParticle);
-        } // i
-        return;
+       for (int i = 0; i < noOfParticles; i++) {
+			Particle newParticle = new Particle();// create multiple with respect to the maximum allowed particles
+			int total = 0;
+			for (int j = 0; j < noOfInputs; j++) {
+				newParticle.data(j, getRandomNumber(lowerLimit, upperLimit));// add random numbers equal to
+																				// noOfInputs in the data array of the
+																				// particle.
+				total += newParticle.data(j);// get the random number allocated at every index in the above step and
+												// add it to total which is set to 0.
+				// Keep on adding the random numbers until all numbers of the array of one
+				// particle are added. Thus total represents the total of all numbers of the
+				// data array.
+			} // j
+			newParticle.pBest(total);// Initially for every particle we set the pBest as the total of all the random
+										// numbers in the data array.
+
+			particles.add(newParticle);// we add the particle to the list
+		} // i
+		return;
     }
 
     private static void PSOAlgorithm() {
         
         int gBestTest = 0;
         Particle aParticle = null;
-        
+        int dataSum=0;
         boolean done = false;
 
         initialize();
@@ -70,25 +98,40 @@ public class AlgosProject {
             //    if the Target value has been found.
             if (epoch < noOfIterations) {
 
-                for (int i = 0; i < noOfParticles; i++) {
-                    aParticle = particles.get(i);
-                      System.out.print("Optimistic time: " +particles.get(i).data(0)+ " ");
-               System.out.print("Most likely time: "+particles.get(i).data(1) + " ");
-                    System.out.println("Pessimistic time: " +particles.get(i).data(2) + " ");
-                System.out.print("PERT Fitness: " +testProblem(i));
-             // j
-            System.out.print("\n");
+               for (int i = 0; i < noOfParticles; i++) {
+					System.out.println("Addition of these values  ");
+					aParticle = particles.get(i);// its is used to get the properties of each particle
+					for (int j = 0; j < noOfInputs; j++) {
 
-                    
-                    if (testProblem(i) == Target) {
-                        done = true;
-                    }
-                } // i
+						if (j < noOfInputs - 1) {
+							dataSum += aParticle.data(j);
 
-                gBestTest = minimum();
+							System.out.print(aParticle.data(j) + " + ");
+						} else {
+							dataSum += aParticle.data(j);// storing sum of 3 inputs
+//							particleMap.put(sum(i, goldP, diamondP, mutualfundsP, gPeriod, dPeriod, mPeriod, bPeriod),
+//									dataSum);
+							particleTreeMap.put(
+									sum(i, goldP, diamondP, mutualfundsP, gPeriod, dPeriod, mPeriod, bPeriod), dataSum);
+							System.out.print(aParticle.data(j) + " = " + dataSum
+									+ " after making the investment as mentioned, will give amount $: ");
+							dataSum = 0;
+
+						}
+					}
+					particles2.add(sum(i, goldP, diamondP, mutualfundsP, gPeriod, dPeriod, mPeriod, bPeriod));
+					System.out.print(sum(i, goldP, diamondP, mutualfundsP, gPeriod, dPeriod, mPeriod, bPeriod) + "\n");
+					if (sum(i, goldP, diamondP, mutualfundsP, gPeriod, dPeriod, mPeriod, bPeriod) == Target) {
+						done = true;
+					}
+				} // i
+
+                gBestTest = Best();
                 aParticle = particles.get(gBest);
                 // if(any particle's pBest value is better than the gBest value, make it the new gBest value.
-                if (Math.abs(Target - testProblem(gBestTest)) < Math.abs(Target - testProblem(gBest))) {
+                if (Math.abs(Target - sum( gBestTest,  goldP,  diamondP,  mutualfundsP,  gPeriod,  dPeriod,  mPeriod,
+			 bPeriod)) < Math.abs(Target - sum( gBest,  goldP,  diamondP,  mutualfundsP,  gPeriod,  dPeriod,  mPeriod,
+			 bPeriod))) {
                     gBest = gBestTest;
                 }
                 displayParticlesJFreeChart();
@@ -117,10 +160,12 @@ public class AlgosProject {
         double vValue = 0.0;
         Particle aParticle = null;
 
-        bestResults = testProblem(gBestindex);
+        bestResults = sum( gBestindex,  goldP,  diamondP,  mutualfundsP,  gPeriod,  dPeriod,  mPeriod,
+			 bPeriod);
 
         for (int i = 0; i < noOfParticles; i++) {
-            testResults = testProblem(i);
+            testResults = sum( i,  goldP,  diamondP,  mutualfundsP,  gPeriod,  dPeriod,  mPeriod,
+			 bPeriod);
             aParticle = particles.get(i);
             vValue = aParticle.velocity() + 2 * new Random().nextDouble() * (aParticle.pBest() - testResults) + 2 * new Random().nextDouble() * (bestResults - testResults);
 
@@ -146,7 +191,8 @@ public class AlgosProject {
             } // j
 
             // Check pBest value.
-            int total = testProblem(i);
+            int total = sum( i,  goldP,  diamondP,  mutualfundsP,  gPeriod,  dPeriod,  mPeriod,
+			 bPeriod);
             if (Math.abs(Target - total) < particles.get(i).pBest()) {
                 particles.get(i).pBest(total);
             }
@@ -155,7 +201,8 @@ public class AlgosProject {
         return;
     }
 
-    private static int testProblem(int index) {
+    private static int sum(int index, int goldP, int diamondP, int mutualfundsP, int gPeriod, int dPeriod, int mPeriod,
+			int bPeriod) {
         int total = 0;
         int diamond = 0;
 		int diamond1 = 0;
@@ -196,7 +243,7 @@ public class AlgosProject {
 		total1 = sum + gold1 + diamond1 + mutualFunds1 + bankInterest;
 		tax = (total1 * 20) / 100;
 		total = total1 - tax;
-        return total=sum;
+        return total;
     }
 
     private static void printSolution() {
@@ -205,7 +252,8 @@ public class AlgosProject {
         Particle particle = null;
         for (i = 0; i < particles.size(); i++) {
             particle = particles.get(i);
-            if (testProblem(i) == Target) {
+            if (sum( i,  goldP,  diamondP,  mutualfundsP,  gPeriod,  dPeriod,  mPeriod,
+			 bPeriod) == Target) {
                 break;
             }
         }
@@ -225,10 +273,18 @@ public class AlgosProject {
     }
 
     private static int getRandomNumber(int low, int high) {
-        return (int) ((high - low) * new Random().nextDouble() + low);
-    }
+        int randomInteger = 0;
+        Random random = new Random();
 
-    private static int minimum() {
+        randomInteger = random.nextInt((high - low) + 1) + low;
+
+        // System.out.println("Random Integer in Java: " + randomInteger);
+
+        return randomInteger;
+
+        // return (int) ((high - low) * new Random().nextDouble() + low);
+    }
+    private static int Best() {
         // Returns an array index.
         int winner = 0;
         boolean foundNewWinner = false;
@@ -238,14 +294,16 @@ public class AlgosProject {
             foundNewWinner = false;
             for (int i = 0; i < noOfParticles; i++) {
                 if (i != winner) {             // Avoid self-comparison.
-                    // The minimum has to be in relation to the Target.
-                    if (Math.abs(Target -testProblem(i)) < Math.abs(Target -testProblem(winner))) {
+                    // The Best has to be in relation to the Target.
+                    if (Math.abs(Target -sum( i,  goldP,  diamondP,  mutualfundsP,  gPeriod,  dPeriod,  mPeriod,
+			 bPeriod)) < Math.abs(Target -sum( winner,  goldP,  diamondP,  mutualfundsP,  gPeriod,  dPeriod,  mPeriod,
+			 bPeriod))) {
                         if (particles.get(i).data(0) <= particles.get(i).data(1) && particles.get(i).data(1) <= particles.get(i).data(2)
                                 && particles.get(i).data(0) <= particles.get(i).data(2)
                                 ) {
                             winner = i;
                             foundNewWinner = true;
-                            System.out.println("Winner is " +winner);
+                            //System.out.println("Winner is " +winner);
                         }
                     }
 
@@ -263,8 +321,42 @@ public class AlgosProject {
   
 
     public static void main(String[] args) {
-        PSOAlgorithm();
-        printSolution();
+        
+        
+        Scanner sc = new Scanner(System.in);
+		//System.out.println("\t\t\t\t\t\t! PARTICLE SWARM OPTIMISATION !");
+		//System.out.println("Enter the number of particles:");
+		int noOfParticles = 100;
+		//System.out.println("Enter the number of iterations:");
+		int noOfIterations = 200;
+		//System.out.println("Enter the lowerlimit of amount to be invested in $:");
+		int lowerlimit = 1000;
+		//System.out.println("Enter the upperlimit of amount to be invested in $::");
+		int upperlimit = 2000;
+		//System.out.println("Enter the target in $:");
+		int target = 2302;
+		//System.out.println("Enter the number of inputs:");
+
+		int noOfInputs = 3;
+		//System.out.println("Enter the percentage of amount to be invested in Gold:");
+		int goldP = 10;
+		//System.out.println("Enter the percentage of amount to be invested in  Diamond:");
+		int diamondP = 10;
+		//System.out.println("Enter the percentage of amount to be invested in  Mutual Funds:");
+		int mutualfundsP = 10;
+		//System.out.println("Enter the duration of time in MONTHS for which you want to invest in Gold:");
+		int gPeriod = 5;
+
+		//System.out.println("Enter the duration of time in MONTHS for which you want to invest in Diamond:");
+		int dPeriod = 5;
+		//System.out.println("Enter the duration of time in MONTHS for which you want to invest in Mutual Funds:");
+		int mPeriod = 5;
+		//System.out.println("Enter the duration of time in MONTHS for which you want to invest in Bank:");
+		int bPeriod = 5;
+              AlgosProject AG = new AlgosProject(target, noOfInputs, noOfParticles, upperlimit, lowerlimit, noOfIterations, goldP, diamondP,
+				mutualfundsP, gPeriod, dPeriod, mPeriod, bPeriod);
+             AG.PSOAlgorithm();
+        AG.printSolution();
         return;
     }
     
--- src/graph/ParticleThread.java
+++ src/graph/ParticleThread.java
@@ -2,6 +2,7 @@ package graph;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Map;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -15,7 +16,6 @@ import org.jfree.data.xy.XYSeriesCollection;
  * @author vighnesh
  */
 public class ParticleThread implements Runnable{
-
     @Override
     public void run() {
         
@@ -29,33 +29,30 @@ public class ParticleThread implements Runnable{
         XYSeriesCollection dataset = new XYSeriesCollection();
 
         XYSeries series1 = new XYSeries("Optimistic values ");
-        XYSeries series4 = new XYSeries("Optimistic gBest ");
-        XYSeries series2 = new XYSeries("Most likely values");
-        XYSeries series5 = new XYSeries("Most likely gBest ");
-        XYSeries series3 = new XYSeries("Pessimistic values");
-        XYSeries series6 = new XYSeries("Pessimistic gBest ");
+//        XYSeries series4 = new XYSeries("Optimistic gBest ");
+//        XYSeries series2 = new XYSeries("Most likely values");
+//        XYSeries series5 = new XYSeries("Most likely gBest ");
+//        XYSeries series3 = new XYSeries("Pessimistic values");
+//        XYSeries series6 = new XYSeries("Pessimistic gBest ");
         
         
     
-        for(Particle p : particles){
+        for(Map.Entry<Integer, Integer> entry : AlgosProject.particleTreeMap.entrySet()){
             
-            series1.add(p.data(0), 1);
-            series2.add(p.data(1), 3);
-            series3.add(p.data(2),5);
-            series4.add(AlgosProject.particles.get(AlgosProject.gBest).data(0),2);
-            series5.add(AlgosProject.particles.get(AlgosProject.gBest).data(1),4);
-            series6.add(AlgosProject.particles.get(AlgosProject.gBest).data(2),6);
-
-                
-                        
+              series1.add(entry.getKey(), entry.getValue());
+//            series2.add(p.data(1), 3);
+//            series3.add(p.data(2),5);
+//            series4.add(AlgosProject.particles.get(AlgosProject.gBest).data(0),2);
+//            series5.add(AlgosProject.particles.get(AlgosProject.gBest).data(1),4);
+//            series6.add(AlgosProject.particles.get(AlgosProject.gBest).data(2),6);
         }
 
         dataset.addSeries(series1);
-        dataset.addSeries(series2);
-        dataset.addSeries(series3);
-        dataset.addSeries(series4);
-        dataset.addSeries(series5);
-        dataset.addSeries(series6);
+//        dataset.addSeries(series2);
+//        dataset.addSeries(series3);
+//        dataset.addSeries(series4);
+//        dataset.addSeries(series5);
+//        dataset.addSeries(series6);
         XYDataset xydataset = dataset;
                         
                         
